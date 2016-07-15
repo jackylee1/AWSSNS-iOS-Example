5c848a3e35daf75b3a84274fe5c9e98ce1c9ef67
Steve <hsu_chiahang@pcgbros.com>
2016-07-15 10:30:34 +0800

    Create SampleProject

---
+++ AWSSNSPushNotification_Sample/AWSSNSPushNotification_Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWSSNSPushNotification_Sample
+//
+//  Created by 許佳航 on 2016/7/15.
+//  Copyright © 2016年 許佳航. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
